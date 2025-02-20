5345ff51f6edddc97c2273c1526a607705ee89dd
Ciro Bassi <ciro.bassi@studio.unibo.it>
2025-02-20 09:28:28 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vampire.io
+
+This is the repository of the OOP project Vampire.io, a demake of "Vampire Survivors".
---
+++ src/main/java/it/unibo/vampireio/Main.java
@@ -0,0 +1,19 @@
+package it.unibo.vampireio;
+
+/**
+ * The main class of the project.
+ */
+final class Main {
+
+    private Main() {
+    }
+
+    /**
+     * The main method of the project.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(final String[] args) {
+        //System.out.println("Hello, World!");
+    }
+}
