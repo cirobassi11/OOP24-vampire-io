c60ed803256d5e238727ae1fab81c01bfd69c992
Paolo Foschini <paolo.foschini2@studio.unibo.it>
2025-04-03 10:22:35 +0200

    SavingManager implemented

--- src/main/java/it/unibo/vampireio/controller/SavingManager.java
+++ src/main/java/it/unibo/vampireio/controller/SavingManager.java
@@ -1,6 +1,13 @@
 package it.unibo.vampireio.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import it.unibo.vampireio.model.Saving;
 
@@ -8,21 +15,40 @@ public class SavingManager {
     private static final String FILE_NAME = System.getProperty("user.home") + File.separator + "vampire-io_savings.sav";
 
     public List<Saving> readSavings() {
-        //todo
-        return null;
+        List<Saving> savings = new ArrayList<>();
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
+            savings = (List<Saving>) in.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("An error occured while reading the list of savings!");
+            e.printStackTrace();
+        }
+        return savings;
     }
 
     public void writeSaving(Saving saving) {
-        //todo
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
+            out.writeObject(saving);
+        } catch (IOException ex) {
+            System.err.println("An error occured while writing a saving!");
+            ex.printStackTrace();
+        }
     }
 
     public void removeSaving(String savingTime) {
-        //todo
+        List<Saving> savings = readSavings();
+        List<SavingData> savingsDatas = getSavingsData();
+        int indexToRemove = 0;
+        for (int i = 0; i < savings.size(); i++) {
+            if (savingsDatas.get(i).getSavingTime() == savingTime) {
+                indexToRemove = i;
+            }
+        }
+        savings.remove(indexToRemove);
     }
 
     public List<SavingData> getSavingsData() {
-        //todo
-        return null;
+        List<SavingData> savingsDatas = new ArrayList<>();
+        return savingsDatas;
     }
 
 }
